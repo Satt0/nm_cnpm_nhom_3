a6c4c042d5784f8b6715efd7cad97b6fc9b7464e
Tan <minhtanbg6601@gmail.com>
2021-12-04 07:32:47 +0700

    format code add db restore tar format

--- backend/src/models/person/resolver.js
+++ backend/src/models/person/resolver.js
@@ -1,53 +1,36 @@
-const {UserCreation,UserQuery,UserUpdate,UserDelete}=require('./data')
-const {authenticate}=require('../../helpers/authen/authenResolver')
+const { UserCreation, UserQuery, UserUpdate, UserDelete } = require("./data");
+const { authenticate } = require("../../helpers/authen/authenResolver");
 
-const root={
-
-}
-const Query={
-    thongTinNhanKhau:authenticate(1,async(_,{input},__,___)=>{
-       
-        const process=new UserQuery()
-        return await process.getOnePerson({ID:input});
-    
-    
-        }),
-    timNhanKhau:authenticate(1,async(_,{input},__,___)=>{
-        const process=new UserQuery()
-        return await process.getManyPerson(input);
-    
-    
-        },)
-}
-const Mutation={
-    taoNhanKhau:async(_,{input},context,__)=>{
-    const idNguoiTao=context.user.ID
-    const process=new UserCreation({...input,idNguoiTao})
+const root = {};
+const Query = {
+  thongTinNhanKhau: authenticate(1, async (_, { input }, __, ___) => {
+    const process = new UserQuery();
+    return await process.getOnePerson({ ID: input });
+  }),
+  timNhanKhau: authenticate(1, async (_, { input }, __, ___) => {
+    const process = new UserQuery();
+    return await process.getManyPerson(input);
+  }),
+};
+const Mutation = {
+  taoNhanKhau: async (_, { input }, context, __) => {
+    const idNguoiTao = context.user.ID;
+    const process = new UserCreation({ ...input, idNguoiTao });
     return await process.CREATE();
+  },
+  capNhatNhanKhau: async (_, { input }, __, ___) => {
+    const process = new UserUpdate(input);
+    return await process.UPDATE();
+  },
+  xoaNhanKHau: async (_, { input }, context, __) => {
+    const idNguoiXoa = context.user.ID;
+    const process = new UserDelete();
+    return await process.deleteOnePerson({ ID: input, idNguoiXoa });
+  },
+  khoiPhucNhanKhau: async (_, { input }, __, ___) => {
+    const process = new UserDelete();
+    return await process.restoreOnePerson({ ID: input });
+  },
+};
 
-
-    },
-    capNhatNhanKhau:async(_,{input},__,___)=>{
-        
-        const process=new UserUpdate(input)
-        return await process.UPDATE();
-    
-    
-        },
-    xoaNhanKHau:async(_,{input},context,__)=>{
-        
-        const idNguoiXoa=context.user.ID
-        const process=new UserDelete()
-        return await process.deleteOnePerson({ID:input,idNguoiXoa});
-    
-    
-        },
-    khoiPhucNhanKhau:async(_,{input},__,___)=>{
-
-        const process=new UserDelete()
-        return await process.restoreOnePerson({ID:input});
-    },
-
-}
-
-module.exports={root,Query,Mutation}
+module.exports = { root, Query, Mutation };
