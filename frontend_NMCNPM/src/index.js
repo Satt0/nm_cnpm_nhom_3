7500cc9cbac4ce649610482c75d31f834684b2da
Tan <minhtanbg6601@gmail.com>
2021-12-11 20:47:50 +0700

    setup apolo client add jwt auto headers

--- frontend_NMCNPM/src/index.js
+++ frontend_NMCNPM/src/index.js
@@ -9,12 +9,41 @@ import * as serviceWorker from "./serviceWorker";
 import { LayoutProvider } from "./context/LayoutContext";
 import { UserProvider } from "./context/UserContext";
 
+// Apollo client setup
+import { ApolloClient, createHttpLink, InMemoryCache ,ApolloProvider,} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+
 ReactDOM.render(
   <LayoutProvider>
     <UserProvider>
       <ThemeProvider theme={Themes.default}>
         <CssBaseline />
+        <ApolloProvider client={client}>
+
         <App />
+        </ApolloProvider>
       </ThemeProvider>
     </UserProvider>
   </LayoutProvider>,
