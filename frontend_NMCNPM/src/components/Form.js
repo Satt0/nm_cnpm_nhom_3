a5cada9980958b957f359b48dd6fc1cdd437f88f
Tan <minhtanbg6601@gmail.com>
2021-12-22 10:30:25 +0700

    fix input

--- frontend_NMCNPM/src/components/Form.js
+++ frontend_NMCNPM/src/components/Form.js
@@ -9,14 +9,15 @@ const Form = ({ listInput, handleChange, state }) => {
       {listInput.map((item) => (
         <div>
          <Autocomplete
-  
+  freeSolo
   options={item?.options?.map(e=>({title:e})) ?? []}
   getOptionLabel={(option) => option.title}
   type={item.type}
   isRequired={item.isRequired}
-  value={state[item.name]}
+  
   
   renderInput={(params) => <TextField {...params}
+  value={state[item.name]}
   onChange={handleChange(item.name)}
   label={item.label} variant="outlined" />}
 />
