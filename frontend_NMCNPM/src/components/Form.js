d2eeaa86f77e789ab24cd84506af89b67f592daf
Tan <minhtanbg6601@gmail.com>
2021-12-22 13:36:00 +0700

    format code

--- frontend_NMCNPM/src/components/Form.js
+++ frontend_NMCNPM/src/components/Form.js
@@ -1,46 +1,29 @@
-import './Form.css'
-import {TextField} from "@material-ui/core"
+import "./Form.css";
+import { TextField } from "@material-ui/core";
 import React from "react";
-import Autocomplete from '@material-ui/lab/Autocomplete';
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
 const Form = ({ listInput, handleChange, state }) => {
   return (
     <form className="form">
       {listInput.map((item) => (
         <div>
-         <Autocomplete
-  freeSolo
-  options={item?.options?.map(e=>({title:e})) ?? []}
-  getOptionLabel={(option) => option.title}
-  type={item.type}
-  isRequired={item.isRequired}
-  
-  
-  renderInput={(params) => <TextField {...params}
-  value={state[item.name]}
-  onChange={handleChange(item.name)}
-  label={item.label} variant="outlined" />}
-/>
-         
-          {/* <TextField list={`dropdown-${item.name}`}
-           onChange={handleChange(item.name)}
-          
-           placeholder={item.placeHolder}
-           type={item.type}
-           isRequired={item.isRequired}
-           value={state[item.name]}
-          label={item.label} variant="outlined" >
-          {item?.options?.map((option) => {
-            console.log(option);
-            return (
-              <MenuItem key={option} value={option}>
-                {option.label}
-              </MenuItem>
-            )
-          })}
-            
-            </TextField> */}
-         
+          <Autocomplete
+            freeSolo
+            options={item?.options?.map((e) => ({ title: e })) ?? []}
+            getOptionLabel={(option) => option.title}
+            type={item.type}
+            isRequired={item.isRequired}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                value={state[item.name]}
+                onChange={handleChange(item.name)}
+                label={item.label}
+                variant="outlined"
+              />
+            )}
+          />
         </div>
       ))}
     </form>
