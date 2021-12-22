32d6fc688defc72d716ad46d4c3718ecf5c7ab60
dung <dungbachdang123@gmail.com>
2021-12-22 13:48:57 +0700

    working on capnhat

--- frontend_NMCNPM/src/api/graphql/query/thong_tin_nhan_khau.js
+++ frontend_NMCNPM/src/api/graphql/query/thong_tin_nhan_khau.js
@@ -1,8 +1,8 @@
 import React, {useState} from "react";
-import { gql } from "graphql";
+import { gql } from "@apollo/client";
 import { useLazyQuery } from "@apollo/client";
 
-const THONG_TIN_NHAN_KHAU = gql`
+export const THONG_TIN_NHAN_KHAU = gql`
 query thongTinNhanKhau($input: Int!){
   thongTinNhanKhau(input: $input) {
     ID
@@ -51,66 +51,66 @@ query thongTinNhanKhau($input: Int!){
 }
 `
 
-function ThongTinNhanKhau(){
-    const [
-        fetchInfor,
-        { data: InforSearchedData, error: InforError },
-      ] = useLazyQuery(THONG_TIN_NHAN_KHAU);
-    const[id, setId] = useState("");  
+// function ThongTinNhanKhau(){
+    // const [
+    //     fetchInfor,
+    //     { data: InforSearchedData, error: InforError },
+    //   ] = useLazyQuery(THONG_TIN_NHAN_KHAU);
+//     const[id, setId] = useState("");  
 
-    return(
-        <div>
-            <input
-          type="text"
-          placeholder="Nhập ID"
-          onChange={(event) => {
-            setId(event.target.value);
-          }}
-        />
-        <button
-          onClick={() => {
-            fetchInfor({
-              variables: {
-                input: id,
-              },
-            });
-          }}
-        >
-          Thông tin nhân khẩu
-        </button>
-        <div>
-          {InforSearchedData && (
-            <div>
-              <h1>ID: {InforSearchedData.thongTinNhanKhau.ID}</h1>
-              <h1>
-                Họ và tên: {InforSearchedData.thongTinNhanKhau.hoTen}
-              </h1>
-              <h1>
-                  Biệt danh: {InforSearchedData.thongTinNhanKhau.bietDanh}
-              </h1>
-              <h1>
-                  Năm sinh: {InforSearchedData.thongTinNhanKhau.namSinh}
-              </h1>
-              <h1>
-                  Giới tính: {InforSearchedData.thongTinNhanKhau.gioiTinh}
-              </h1>
-              <h1>
-                  Dân tộc: {InforSearchedData.thongTinNhanKhau.danToc}
-              </h1>
-              <h1>
-                  Tôn giáo: {InforSearchedData.thongTinNhanKhau.tonGiao}
-              </h1>
-              <h1>
-                  Quốc tịch: {InforSearchedData.thongTinNhanKhau.quocTich}
-              </h1>
-              <h1>
-                  Nguyên quán: {InforSearchedData.thongTinNhanKhau.nguyenQuan}
-              </h1>
-            </div>
-          )}
-          {InforError && <h1> There was an error fetching the data</h1>}
-        </div>
-        </div>
-    )
-}
-export default ThongTinNhanKhau
+//     return(
+//         <div>
+//             <input
+//           type="text"
+//           placeholder="Nhập ID"
+//           onChange={(event) => {
+//             setId(event.target.value);
+//           }}
+//         />
+        // <button
+        //   onClick={() => {
+        //     fetchInfor({
+        //       variables: {
+        //         input: id,
+        //       },
+        //     });
+        //   }}
+        // >
+        //   Thông tin nhân khẩu
+        // </button>
+//         <div>
+//           {InforSearchedData && (
+//             <div>
+//               <h1>ID: {InforSearchedData.thongTinNhanKhau.ID}</h1>
+//               <h1>
+//                 Họ và tên: {InforSearchedData.thongTinNhanKhau.hoTen}
+//               </h1>
+//               <h1>
+//                   Biệt danh: {InforSearchedData.thongTinNhanKhau.bietDanh}
+//               </h1>
+//               <h1>
+//                   Năm sinh: {InforSearchedData.thongTinNhanKhau.namSinh}
+//               </h1>
+//               <h1>
+//                   Giới tính: {InforSearchedData.thongTinNhanKhau.gioiTinh}
+//               </h1>
+//               <h1>
+//                   Dân tộc: {InforSearchedData.thongTinNhanKhau.danToc}
+//               </h1>
+//               <h1>
+//                   Tôn giáo: {InforSearchedData.thongTinNhanKhau.tonGiao}
+//               </h1>
+//               <h1>
+//                   Quốc tịch: {InforSearchedData.thongTinNhanKhau.quocTich}
+//               </h1>
+//               <h1>
+//                   Nguyên quán: {InforSearchedData.thongTinNhanKhau.nguyenQuan}
+//               </h1>
+//             </div>
+//           )}
+//           {InforError && <h1> There was an error fetching the data</h1>}
+//         </div>
+//         </div>
+//     )
+// }
+// export default ThongTinNhanKhau
--- frontend_NMCNPM/src/pages/sua-nhan-khau/subs/sua-nhan-khau.js
+++ frontend_NMCNPM/src/pages/sua-nhan-khau/subs/sua-nhan-khau.js
@@ -1,13 +1,276 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {useParams } from 'react-router-dom';
+import { THONG_TIN_NHAN_KHAU } from '../../../api/graphql/query/thong_tin_nhan_khau';
+import { useLazyQuery, useQuery } from "@apollo/client";
+import moment from 'moment'
+import Form from "../../../components/Form";
 export default function EditOne() {
     const params = useParams();
-    console.log(params)
+    const id = Object.values(params)[0]
+    const 
+        { data: InforSearchedData, error: InforError, loading }
+       = useQuery(THONG_TIN_NHAN_KHAU, {
+        variables: {
+          input: parseInt(id),
+        },
+      });
+      const listInput=[
+        {
+        label:"Họ Tên",
+        name:"hoTen",
+        isRequired:true,
+        defaultValue:"",
+        type:"text",
+        placeHolder:'enter your name',
+        
+        },
+        {
+          label:"Biệt danh",
+          name:"bietDanh",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền biệt danh',
+        },
+        {
+          label:"Năm sinh",
+          name:"namSinh",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền năm sinh',
+        },
+        {
+          label:"Giới tính",
+          name:"gioiTinh",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền giới tính',
+        },  
+        {
+          label:"Nguyên quán",
+          name:"nguyenQuan",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền nguyên quán',
+        }, 
+        {
+          label:"Dân tộc",
+          name:"danToc",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền dân tộc',
+          options:[
+            "Kinh",
+            "Thai"
+          ]
+        },   
+        {
+          label:"Tôn giáo",
+          name:"tonGiao",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền tôn giáo',
+        },  
+        {
+          label:"Quốc tịch",
+          name:"quocTich",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền quốc tịch',
+        },  
+        {
+          label:"Số hộ chiếu",
+          name:"soHoChieu",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền số hộ chiếu',
+        }, 
+        {
+          label:"Nơi thường trú",
+          name:"noiThuongTru",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền nơi thường trú',
+        },
+        {
+          label:"Địa chỉ hiện nay",
+          name:"diaChiHienNay",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền địa chỉ hiện nay',
+        }, 
+        {
+        label:"Nghề nghiệp",
+        name:"ngheNghiep",
+        isRequired:true,
+        type:"text",
+        defaultValue:"",
+        placeholder:'điền nghề nhiệp',
+        },
+        
+        {
+          label:"Mã nhân khẩu",
+          name:"maNhanKhau",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền mã nhân khẩu',
+        },  
+        {
+          label:"Nơi sinh",
+          name:"noiSinh",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền nơi sinh',
+        },
+        {
+          label:"Biết tiếng dân tộc",
+          name:"bietTiengDanToc",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền tiếng dân tộc',
+        },  
+        {
+          label:"Địa chỉ mới",
+          name:"diaChiMoi",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền địa chỉ mới',
+        }, 
+        {
+          label:"Lý do chuyển đến",
+          name:"lyDoChuyenDen",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền lý do chuyển đến',
+        },
+        {
+          label:"Lý do chuyển đi",
+          name:"lyDoChuyenDi",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền lý do chuyển đi',
+        }, 
+        {
+          label:"Ngày chuyển đến",
+          name:"ngayChuyenDen",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền ngày chuyển đến',
+        },   
+        {
+          label:"Ngày chuyển đi",
+          name:"ngayChuyenDi",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền ngày chuyển đi',
+        },
+        {
+          label:"Ngày tạo",
+          name:"ngayTao",
+          isRequired:true,
+          type:"text",
+          defaultValue:moment().format('YYYY-MM-DD'),
+          placeholder:'điền ngày tạo',
+        },  
+        {
+          label:"Nơi làm việc",
+          name:"noiLamViec",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền nơi làm việc',
+        },   
+        {
+          label:"Tiền án",
+          name:"tienAn",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền tiền án',
+        },   
+        
+        {
+          label:"Trình độ chuyên môn",
+          name:"trinhDoChuyenMon",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền trình độ',
+        },    
+        {
+          label:"Trình độ học vấn",
+          name:"trinhDoHocVan",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền trình độ',
+        }, 
+        {
+          label:"Trình độ ngoại ngữ",
+          name:"trinhDoNgoaiNgu",
+          isRequired:true,
+          type:"text",
+          defaultValue:"",
+          placeholder:'điền trình độ',
+        },
+        ];
+        const [state,setState]=useState(()=>{
+            const nhanKhau={};
+            listInput.forEach(p=>{
+              nhanKhau[p.name]=p.defaultValue ?? "";
+            })
+             return nhanKhau;
+          })
+          const handleChange=(key)=>{
+            return (e)=>{
+              const value=e.target.value;
+              setState(old=>({...old,[key]:value}))
+            }
+          }  
+          useEffect(()=>{
+            if(loading) return 
+           if(InforSearchedData)
+          {
+           setState(old=>({...old,...InforSearchedData.thongTinNhanKhau}))
+          }
+          
+          },[loading,InforSearchedData])
+          
     return (
         <div>
-            <p>{JSON.stringify(params)}</p>
-          <button onClick={()=> console.log(params)}>click</button>
-
+           {/* <button
+          onClick={() => {
+            fetchInfor({
+              variables: {
+                input: parseInt(id),
+              },
+            }).catch(e=>{
+                console.log(e.message)
+              });
+            //   console.log(InforSearchedData)
+              
+          }}
+        >
+          Thông tin nhân khẩu
+        </button> */}
+        <Form listInput={listInput} state={state}  handleChange={handleChange}/>
         </div>
     )
 }
