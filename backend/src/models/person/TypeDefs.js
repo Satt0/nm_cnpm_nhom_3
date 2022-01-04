19d6ac9ea1e6553cbd8c09d34f97f0b1165f549e
Tan <minhtanbg6601@gmail.com>
2022-01-04 23:06:56 +0700

    da xoa is optional

--- backend/src/models/person/TypeDefs.js
+++ backend/src/models/person/TypeDefs.js
@@ -166,7 +166,7 @@ input inputTimNhanKhau{
     limit:Int!
     offset:Int!
     name:String!
-    daXoa:Boolean!
+    daXoa:Boolean
 }
 extend type Query {
     thongTinNhanKhau(input:Int!):NhanKhau!
